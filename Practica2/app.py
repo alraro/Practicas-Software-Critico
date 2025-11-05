59964dd6ff82ce4264b5a7d41b759918991d642a
alraro <alfonsoramosrojas@gmail.com>
2025-11-05 13:32:59 +0100

    Added nuevo and listar enpoints

--- Practica2/app.py
+++ Practica2/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from redis import Redis, RedisError
+from datetime import datetime
 import os
 import socket
 
@@ -22,6 +23,36 @@ def hello():
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+
+@app.route("/nuevo")
+def nuevo():
+    dato = request.args.get('dato')
+    fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        if (dato == "") or (dato is None):
+            respuesta = "Dato no proporcionado, no se agregó nada."
+        else:
+            redis.hset("mediciones", fecha, dato)
+            respuesta = "Dato '"+dato+"' agregado a la lista."
+    except RedisError:
+        respuesta = "No se pudo conectar a Redis, dato no agregado."
+    finally:
+        return f"<p>{respuesta}</p>"
+
+@app.route("/listar")
+def listar():
+    try:
+        valores = redis.hgetall("mediciones")
+        if not valores:
+            return "<p>No hay datos almacenados.</p>"
+        html = "<h3>Lista de Mediciones:</h3><ul>"
+        for fecha, dato in valores.items():
+            html += f"<li>{fecha.decode('utf-8')}: {dato.decode('utf-8')}</li>"
+        html += "</ul>"
+        return html
+    except RedisError:
+        return "<p>No se pudo conectar a Redis para listar los datos.</p>"
 
 if __name__ == "__main__":
     PORT = os.getenv('PORT', 80)
