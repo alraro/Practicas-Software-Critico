83427b5e0b0fce90f26e5c62ab8bb0cb2e075b18
alraro <alfonsoramosrojas@gmail.com>
2025-11-19 14:12:51 +0100

    Added threshold to txt file

--- Practica2/app.py
+++ Practica2/app.py
@@ -14,59 +14,65 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+	try:
+		visits = redis.incr("counter")
+	except RedisError:
+		visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+	html = "<h3>Hello {name}!</h3>" \
+		   "<b>Hostname:</b> {hostname}<br/>" \
+		   "<b>Visits:</b> {visits}"
+	return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 
 @app.route("/nuevo")
 def nuevo():
-    res = []
-    data = request.args.get('dato')
-    try:
-        if (data == "") or (data is None):
-            res.append({"error": "Empty input error"})
-        else:
-            redis.execute_command('TS.ADD', 'temperature', '*', data)
-            res.append({"data": data, "message": f"New input {data} added."})
-    except RedisError:
-        res.append({"error": "Redis connection error"})
-    finally:
-        return res
+	res = []
+	data = request.args.get('dato')
+	try:
+		if (data == "") or (data is None):
+			res.append({"error": "Empty input error"})
+		else:
+			redis.execute_command('TS.ADD', 'temperature', '*', data)
+			res.append({"data": data, "message": f"New input {data} added."})
+	except RedisError:
+		res.append({"error": "Redis connection error"})
+	finally:
+		return res
 @app.route("/listar")
 def listar():
-    res = []
-    try:
-        valores = redis.execute_command('TS.RANGE', 'temperature', '-', '+')
-        for dato in valores:
-            res.append({"timestamp": dato[0], "value": float(dato[1].decode('utf-8'))})
-    except RedisError:
-        res.append({"error": "Redis connection error"})
-    finally:
-        return res
-    
+	res = []
+	try:
+		valores = redis.execute_command('TS.RANGE', 'temperature', '-', '+')
+		for dato in valores:
+			res.append({"timestamp": dato[0], "value": float(dato[1].decode('utf-8'))})
+	except RedisError:
+		res.append({"error": "Redis connection error"})
+	finally:
+		return res
+	
 @app.route("/detectar")
 def detectar():
-    res = []
-    data = request.args.get('dato')
-    try:
-        if (data == "") or (data is None):
-            res.append({"error": "Empty input error"})
-        else:
-            temp = float(data)
-            
-    except RedisError:
-        res.append({"error": "Redis connection error"})
-    finally:
-        return res
+	res = []
+	data = request.args.get('dato')
+	window_size = 3
+	try:
+		if (data == "") or (data is None):
+			res.append({"error": "Empty input error"})
+		else:
+			window = redis.execute_command('TS.RANGE', 'temperature', f'-{window_size}', '-1')
+			if (len(window) < window_size):
+				res.append({"error": "Not enough data for anomaly detection"})
+				return res
+			
+			redis.execute_command('TS.ADD', 'temperature', '*', data)
+			res.append({"data": data, "message": f"New input {data} added."})
+	except RedisError:
+		res.append({"error": "Redis connection error"})
+	finally:
+		return res
 
 if __name__ == "__main__":
-    PORT = os.getenv('PORT', 80)
-    print("PORT: "+str(PORT))
-    app.run(host='0.0.0.0', port=PORT)
+	PORT = os.getenv('PORT', 80)
+	print("PORT: "+str(PORT))
+	app.run(host='0.0.0.0', port=PORT)
